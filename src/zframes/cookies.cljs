237afc44fb5ab000f7498fb98df6032724ab3784
Vyacheslav Mikushev <vyacheslav.mik.8@gmail.com>
2019-07-01 16:36:31 +0300

    Fixed cookies on mobile safari and improved cookie interop

--- src/zframes/cookies.cljs
+++ src/zframes/cookies.cljs
@@ -4,12 +4,26 @@
             [re-frame.core :as rf]
             [cljs.reader :as reader]))
 
+(defn get-cookie
+  "Returns the cookie after parsing it with cljs.reader/read-string."
+  [k]
+  (try
+    (let [v (.get goog.net.cookies (name k))
+          v (if v
+              (js/window.atob v)
+              "nil")]
+      (reader/read-string v))
+    (catch :default e
+      (js/console.error (str "Get cookie (" k ") error: " e))
+      nil)))
 
-(defn get-cookie "Returns the cookie after parsing it with cljs.reader/read-string."
-  [k] (reader/read-string (or (.get goog.net.cookies (name k)) "nil")))
-
-(defn set-cookie "Stores the cookie value using pr-str."
-  [k v] (.set goog.net.cookies (name k) (pr-str v)))
+;; For some reason on mobile safari cookie is truncated
+;; before comma. But not in all situations. I decided
+;; to use base64 to dodge many errors with signs.
+(defn set-cookie
+  "Stores the cookie value using pr-str."
+  [k v]
+  (.set goog.net.cookies (name k) (js/window.btoa (pr-str v))))
 
 (defn remove! [k] (.remove goog.net.cookies (name k)))
 
